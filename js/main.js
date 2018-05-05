54897ebe6bb04815de4ad9d3d5771a5f4eaa83b9
Igor Todorovic <igor333m@yahoo.com>
2018-05-05 16:45:13 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-
+let map;
 
 function initMap() {
-	let map;
 	// Constructor creates new map - only center and zoom are required
 	map = new google.maps.Map(document.getElementById('map'), {
 		center:{lat: 34.1981063, lng: -118.4684912},
 		zoom: 13
+	});
+
+	// North Hollywood Station Marker
+	let noho = {lat: 34.1689208,lng: -118.3767172};
+	let nohoMarker = new google.maps.Marker({
+		position: noho,
+		map: map,
+		title: 'Metro RedLine North Hollywood Station'
 	});
 }
 
